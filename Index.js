2089228930d337d9781590de36c2215759e3a892
Tanishka <guptatanishka2008@gmail.com>
2024-04-11 21:14:55 +0530

    first commit

---
+++ Index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get( '/twitter', (req, res) => {
+ res.send("Tanishka is sending!")
+})
+
+app.get('/youtube',(req, res) => {
+ res.send("I am strong")
+})
+
+app.listen(process.env.PORT, () => {
+ console.log(`Example app listening on port ${port}`)
+})
